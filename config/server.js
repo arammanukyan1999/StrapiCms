a73da4140933c013e99f76cff37ec862b6ddbbc4
arammanukyan1999 <aramman1999@gmail.com>
2020-12-04 18:31:06 +0400

    create test api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a42d044f11ce707cd7faf6bad1f38e00'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd904d617-115b-434b-a1f2-a18d73750776'
+};
